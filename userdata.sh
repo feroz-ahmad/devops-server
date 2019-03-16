2c4e270d90bcb22cff51e4d660a975599fcd7160
Feroz Ahmad <f29ahmad@gmail.com>
2019-03-16 23:27:36 +0530

    Remove c

---
+++ userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "DistelliAccessToken: '1VINJ4O2CXOHV00B6KWC3R906I'" > /etc/distelli.yml
+echo "DistelliSecretKey: '118ucudjcdp808iqk0ngfktvykt2ytu76gd4a'" >> /etc/distelli.yml
+echo "Environments:" >> /etc/distelli.yml
+echo "  - dev" >> /etc/distelli.yml
+wget -qO- https://pipelines.puppet.com/download/client | sh
+/usr/local/bin/distelli agent install -conf /etc/distelli.yml
+/usr/local/bin/distelli login -conf /etc/distelli.yml

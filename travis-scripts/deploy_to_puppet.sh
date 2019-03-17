5e50e6fb22a8a57888738a5ec96b11d007f48ab8
Feroz Ahmad <f29ahmad@gmail.com>
2019-03-17 16:59:38 +0530

    Deploy only on master

---
+++ travis-scripts/deploy_to_puppet.sh
@@ -0,0 +1,6 @@
+echo "-- Deploying to Puppet --"
+wget -qO- https://pipelines.puppet.com/download/client | sh
+echo "DistelliAccessToken:" " '$DistelliAccessToken'" >> ~/distelli.yml
+echo "DistelliSecretKey:" "'$DistelliSecretKey'" >> ~/distelli.yml
+/usr/local/bin/distelli login -conf ~/distelli.yml
+/usr/local/bin/distelli deploy -q -y -m "Deployment from Travis-CI" --env dev
